ecf9366e214ec4d3475dc8ca6031b3e8759d5045
Aliaksei <156023873+azhemoita@users.noreply.github.com>
2026-01-12 17:44:22 +0300

    feat: add OpenAPI generator and enhance API annotations

--- src/main/java/com/javarush/stockvault/controller/UserController.java
+++ src/main/java/com/javarush/stockvault/controller/UserController.java
@@ -5,12 +5,15 @@ import com.javarush.stockvault.dto.LoginResponse;
 import com.javarush.stockvault.dto.UserRegistrationRequest;
 import com.javarush.stockvault.entity.User;
 import com.javarush.stockvault.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Users", description = "User registration and authentication")
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -21,6 +24,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Register new user")
     @PostMapping("/register")
     public ResponseEntity<User> createUser(@Valid @RequestBody UserRegistrationRequest request) {
         User savedUser = userService.createUser(
@@ -31,6 +35,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
+    @Operation(summary = "User login")
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
         LoginResponse response = userService.login(
